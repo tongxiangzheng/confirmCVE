0e3fa565844401490546947e4d13ec9f31a1be3d
txz <920504276@qq.com>
2024-05-21 20:20:07 +0800

    connect freature

--- src/frontEnd/server.py
+++ src/frontEnd/server.py
@@ -1,5 +1,7 @@
 import socket
-
+import json
+def solve(packageList):
+    print(packageList)
 def server():
     s = socket.socket()
     port = 8342
@@ -7,6 +9,14 @@ def server():
     s.listen(5)
     while True:
         c,addr = s.accept()
-        print(c.recv(1024))
+        lenInfo=c.recv(4)
+        length=int.from_bytes(lenInfo, byteorder='big')
+        data=b""
+        while length!=0:
+            recvInfo=c.recv(min(1024,length))
+            data=data+recvInfo
+            length-=len(recvInfo)
+        data=data.decode()
+        solve(json.loads(data))
         c.close()
 server()
