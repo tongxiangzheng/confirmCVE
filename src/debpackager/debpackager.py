9bf86a7fa8e9ef2a9f31a1132d60335eea9574bc
佟祥正 <13956394+tong-xiangzheng@user.noreply.gitee.com>
2024-09-13 15:22:08 +0800

    update

--- src/debpackager/debpackager.py
+++ src/debpackager/debpackager.py
@@ -1,3 +1,18 @@
+import os
+import tarfile
 
-def getBuildInfo():
-	pass
+
+def unzip(zipfile,toPath):
+	with tarfile.open(zipfile) as f:
+		f.extractall(toPath)
+		return f.getmembers()[0].name
+
+
+
+def getBuildInfo(srcFile,srcFile2,osType,osDist,arch):
+	os.system("cp {} files".format(srcFile))
+	projectName=unzip(srcFile,"files")
+	projectPath=os.path.join("files",projectName)
+	if srcFile2:
+		unzip(srcFile2,projectPath)
+	
---
+++ src/debpackager/test.py
@@ -0,0 +1,2 @@
+import debpackager
+debpackager.getBuildInfo("network-manager_1.22.10.orig.tar.xz","network-manager_1.22.10-1ubuntu2.4.debian.tar.xz","Ubuntu","boinc","amd64")
