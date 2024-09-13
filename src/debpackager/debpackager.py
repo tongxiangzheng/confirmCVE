1715fb19f8c7cf0a25edfa0af0614efffc6c93ce
Intern Tong Xiangzheng <920504276@qq.com>
2024-09-13 16:46:58 +0800

    update

--- src/debpackager/debpackager.py
+++ src/debpackager/debpackager.py
@@ -7,12 +7,19 @@ def unzip(zipfile,toPath):
 		f.extractall(toPath)
 		return f.getmembers()[0].name
 
-
+def builddebPackage(origName,projectName):
+	cmd='docker build -t txz_debbuild --build-arg ORIGNAME="{}" --build-arg PROJECTNAME="{}" .'.format(origName,projectName)
+	#os.system(cmd)
+	print(cmd)
 
 def getBuildInfo(srcFile,srcFile2,osType,osDist,arch):
 	os.system("cp {} files".format(srcFile))
-	projectName=unzip(srcFile,"files")
+	projectName=unzip(srcFile,"files/")
+	print(projectName)
 	projectPath=os.path.join("files",projectName)
 	if srcFile2:
 		unzip(srcFile2,projectPath)
+	builddebPackage(srcFile,projectName)
+
+
 	
--- src/debpackager/test.py
+++ src/debpackager/test.py
@@ -1,2 +1,2 @@
 import debpackager
-debpackager.getBuildInfo("network-manager_1.22.10.orig.tar.xz","network-manager_1.22.10-1ubuntu2.4.debian.tar.xz","Ubuntu","boinc","amd64")
+debpackager.getBuildInfo("sed_4.9.orig.tar.xz","sed_4.9-2build1.debian.tar.xz","Ubuntu","boinc","amd64")
