9803fa322ea7b0c42f3558087c390be8b22aef13
佟祥正 <13956394+tong-xiangzheng@user.noreply.gitee.com>
2024-09-14 16:45:51 +0800

    OK

--- src/debpackager/debpackager.py
+++ src/debpackager/debpackager.py
@@ -6,20 +6,34 @@ def unzip(zipfile,toPath):
 	with tarfile.open(zipfile) as f:
 		f.extractall(toPath)
 		return f.getmembers()[0].name
-
-def builddebPackage(origName,projectName):
-	cmd='docker build -t txz_debbuild --build-arg ORIGNAME="{}" --build-arg PROJECTNAME="{}" .'.format(origName,projectName)
-	#os.system(cmd)
-	print(cmd)
+def loadFile(filePath):
+	with open(filePath,"r") as f:
+		data=f.read()
+		return data
+	
+def builddebPackage(origName,projectName,osType,osDist,arch):
+	cmd='docker build --output=./buildinfos --target=buildinfo --build-arg ORIGNAME="{}" --build-arg PROJECTNAME="{}" --build-arg SYSTEM_NAME="{}" --build-arg SYSTEM_VERSION="{}" --build-arg BUILD_ARCH="{}" .'.format(origName,projectName,osType,osDist,arch)
+	os.system(cmd)
+	
 
 def getBuildInfo(srcFile,srcFile2,osType,osDist,arch):
+	os.system("rm files -rf")
+	os.system("mkdir files")
+	os.system("rm buildinfos -rf")
+	os.system("mkdir buildinfos")
 	os.system("cp {} files".format(srcFile))
 	projectName=unzip(srcFile,"files/")
-	print(projectName)
 	projectPath=os.path.join("files",projectName)
 	if srcFile2:
 		unzip(srcFile2,projectPath)
-	builddebPackage(srcFile,projectName)
-
+	srcFileName=os.path.basename(srcFile)
+	builddebPackage(srcFileName,projectName,osType,osDist,arch)
+	buildInfosPath='buildinfos'
+	res=[]
+	for file in os.listdir(buildInfosPath):
+		if os.path.isfile(os.path.join(buildInfosPath, file)):
+			buildInfoFile=os.path.join(buildInfosPath,file)
+			res.append(loadFile(buildInfoFile))
+	return res
 
 	
--- src/debpackager/test.py
+++ src/debpackager/test.py
@@ -1,2 +1,3 @@
 import debpackager
-debpackager.getBuildInfo("sed_4.9.orig.tar.xz","sed_4.9-2build1.debian.tar.xz","Ubuntu","boinc","amd64")
+res=debpackager.getBuildInfo("sed_4.9.orig.tar.xz","sed_4.9-2build1.debian.tar.xz","ubuntu","jammy","amd64")
+print(res)
